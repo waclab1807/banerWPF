492f820445609562ebacc3428166a421ef962a14
Mariusz Konior <koniormariusz90@o2.pl>
2016-10-05 19:45:46 +0200

    NEW: class canvas

---
+++ PaintFPMariuszKonior/Canvas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintFPMariuszKonior
+{
+    public class CanvasDimensions
+    {
+        public double width { get; set; }
+        public double height { get; set; }
+    }
+}
